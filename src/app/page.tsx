96f1ffe4a4f9a3a40577878aba2390d97a104be8
Sankalpa-baral <sankalpabaral70@gmail.com>
2025-05-06 14:38:33 +0545

    feat: Refactor Value Proposition Section and add new components
    - Updated Value Proposition Section to include new layout and components.
    - Introduced BentoGrid and BentoGridItem for better grid management.
    - Added simple animated testimonials component for showcasing user feedback.
    - Created shape landing hero component for enhanced visual appeal.
    - Implemented various skeleton loaders for improved user experience during loading states.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import ServicesSection from '@/components/sections/services-section';
 import ValuePropositionSection from '@/components/sections/value-proposition-section';
 // import VisionGoalSection from '@/components/sections/vision-goal-section'; // Removed import
 import MyProjectsSection from '@/components/sections/my-projects-section'; // Import the new MyProjectsSection
-import TestimonialsSection from '@/components/sections/testimonials-section';
+import { TestimonialsSectionBasic } from '@/components/sections/testimonials-section';
 import TeamSection from '@/components/sections/team-section';
 import NewsletterSection from '@/components/sections/newsletter-section';
 import FooterSection from '@/components/sections/footer-section';
 import Header from '@/components/layout/header';
 import BlurryCursor from '@/components/cursor';
+import { HeroGeometric } from '@/components/ui/shape-landing-hero';
 // import FloatingCTAButton from '@/components/layout/floating-cta-button'; // Import the new component // Commented out Floating CTA
 
 export const metadata: Metadata = {
@@ -26,11 +27,15 @@ export default function Home() {
       {/* Removed pt-4 and md:pt-8 for less top padding */}
       <main className="flex-grow pb-8 md:pb-16 flex flex-col items-center justify-center">
         <HeroSection/>
-        <ServicesSection />
+        {/* <ServicesSection /> */}
         <ValuePropositionSection />
         {/* <VisionGoalSection /> Removed component usage */}
-        <MyProjectsSection /> {/* Use the new MyProjectsSection */}
-        <TestimonialsSection />
+        <HeroGeometric badge="SevenX"
+            title1 = "Elevate Your"
+            title2 = "Business" />
+        {/* <MyProjectsSection />  */}
+        {/* <TestimonialsSection /> */}
+        <TestimonialsSectionBasic />
         <TeamSection />
         <NewsletterSection />
       </main>
